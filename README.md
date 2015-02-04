0ceb8b6b2591f72379e7a0c3dab41940a35ed5b7
dolgarev <o.dolgarev@gmail.com>
2015-02-04 17:44:22 +0200

    minor improvements bumped version number to 1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # accounts-paypal
 
 A login service for Paypal. See the [project page](https://www.meteor.com/accounts) on Meteor Accounts for more details.
+
+Install
+-----------
+```
+meteor add liberation:accounts-paypal
+```
--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'liberation:accounts-paypal',	
   summary: 'Login service for PayPal accounts',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/dolgarev/meteor-accounts-paypal'
 });
 
 Package.onUse(function(api) {
-  api.use('accounts-base', ['client', 'server']);
+  api.use('accounts-base@1.1.3', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('liberation:paypal-login', ['client', 'server']);
+  api.use('accounts-oauth@1.1.3', ['client', 'server']);
+  api.use('liberation:paypal-login@1.0.0', ['client', 'server']);
 
   api.addFiles('paypal_login_button.css', 'client');
 
