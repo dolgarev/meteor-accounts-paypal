4cd1d009b6dc8cd46c41a52d704c2b72a6db4dc0
dolgarev <o.dolgarev@gmail.com>
2015-03-18 18:02:42 +0200

    Revert "fixed depending"
    This reverts commit cc75425299906a2c3cf829f46cc3bed92da643fc.

--- package.js
+++ package.js
@@ -1,22 +1,19 @@
 Package.describe({
-	summary: "A paypal package that helps make restful API calls to Paypal",
-	version: "1.2.0",
-	name: "limemakers:paypal",
-	git: "https://github.com/LimeMakers/meteor-paypal.git"
+  name: 'liberation:accounts-paypal',	
+  summary: 'Login service for PayPal accounts',
+  version: '1.0.3',
+  git: 'https://github.com/dolgarev/meteor-accounts-paypal'
 });
 
-Npm.depends({
-	"paypal-rest-sdk": "1.5.1"
-});
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2');
+  api.use('accounts-base', ['client', 'server']);
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('liberation:paypal-login@1.0.3', ['client', 'server']);
 
-Package.on_use(function(api) {
-	api.versionsFrom("1.0");
-	api.use(["meteorhacks:npm"], "server");
-	api.use(["templating"], "client");
-	api.add_files("paypal.js", ["client", "server"]);
-	api.add_files(["paypal_credit_card_form.html", "paypal_credit_card_form.js"], "client");
-});
+  api.export('PaypalLogin')
 
-Package.on_test(function(api) {
-	//need to add some tests
+  api.addFiles('paypal_login_button.css', 'client');
+  api.addFiles("paypal.js");
 });
